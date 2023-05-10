9b89ff4277409078ba856d830bc823c9ebdcd810
gmpassos <gracilianomp@gmail.com>
2023-05-10 02:57:06 -0300

    v2.1.7

--- lib/src/intl_messages_base.dart
+++ lib/src/intl_messages_base.dart
@@ -234,7 +234,7 @@ class IntlResourceDiscover {
 /// Represents a message table with keys and values.
 class IntlMessages {
   // ignore: constant_identifier_names
-  static const String VERSION = '2.1.6';
+  static const String VERSION = '2.1.7';
 
   static String normalizePackageName(String packageName) =>
       packageName.toLowerCase().trim();
